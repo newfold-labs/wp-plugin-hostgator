d3d72e922ed786c01a8e5b1625f6ec72945215af
Evan Mullins <evanrm@gmail.com>
2021-12-03 09:47:19 -0500

    initial plugin scaffolding

---
+++ hostgator-wordpress-plugin.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * HostGator WordPress Plugin
+ *
+ * @package           HostGatorWordPressPlugin
+ * @author            Evan Mullins
+ * @copyright         Copyright 2021 by Evan Mullins - All rights reserved.
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       HostGator WordPress Plugin
+ * Plugin URI:        
+ * Description:       WordPress plugin that integrates your WordPress site with the HostGator control panel, including performance, security, and update features.
+ * Version:           1.0
+ * Requires PHP:      7.0
+ * Requires at least: 5.0
+ * Author:            Evan Mullins
+ * Author URI:        https://evanmullins.com
+ * Text Domain:       hostgator-wordpress-plugin
+ * Domain Path:       /languages
+ * License:           GPL 2.0 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
---
+++ tests/cypress/support/index.js
@@ -0,0 +1,30 @@
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+import 'cypress-axe';
+import './commands';
+
+Cypress.on('window:before:load', (win) => {
+	// See https://github.com/cypress-io/cypress-example-recipes/tree/master/examples/stubbing-spying__window-fetch#readme
+	delete win.fetch
+});
+
+Cypress.Cookies.defaults({
+	preserve: /wp|wordpress/,
+});
+
+before(() => {
+	cy.login(Cypress.env('wpUsername'), Cypress.env('wpPassword'));
+});
