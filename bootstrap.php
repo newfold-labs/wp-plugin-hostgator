757f5d4ef526cb90e69436033b75ba6c65bdaf0a
Evan Mullins <evanrm@gmail.com>
2022-03-28 19:01:30 -0400

    Feature/coming soon module (#25)
    * migrate coming soon to using module
    
    * upgrade to latest coming soon module
    
    * update to latest coming soon module
    
    * update to latest coming soon module
    
    * use latest version of coming soon module to migrate admin bar styles inline
    
    * version bump
    
    * update coming soon module, remove unneeded default args in container, make strings translatable, upgrade routine to use nfd name spaced coming soon option.
    
    * update test, update coming_soon option in settings controller
    
    * WordPress I18N triggered by commit

--- bootstrap.php
+++ bootstrap.php
@@ -9,6 +9,9 @@ namespace HostGator;
 
 use Hostgator\UpgradeHandler;
 use WP_Forge\WPUpdateHandler\PluginUpdater;
+use NewfoldLabs\WP\ModuleLoader\Container;
+use NewfoldLabs\WP\ModuleLoader\Plugin;
+use function NewfoldLabs\WP\ModuleLoader\container as setContainer;
 
 // Composer autoloader
 if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
@@ -19,6 +22,64 @@ if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 	}
 	return;
 }
+
+
+/*
+ * Initialize coming soon module via container
+ */
+$nfd_module_container = new Container();
+// Set plugin to container
+$nfd_module_container->set(
+	'plugin',
+	$nfd_module_container->service(
+		function() {
+			return new Plugin(
+				array(
+					'id'   => 'hostgator',
+					'file' => HOSTGATOR_PLUGIN_FILE,
+				)
+			);
+		}
+	)
+);
+
+// Set coming soon values
+$nfd_module_container->set(
+	'comingsoon',
+	array(
+		'admin_app_url'       => admin_url( 'admin.php?page=hostgator#/home' ),
+		'template_h1'         => __( 'A New WordPress Site', 'wp-plugin-hostgator' ),
+		'template_h2'         => __( 'Coming Soon!', 'wp-plugin-hostgator' ),
+		'template_footer_t'   => sprintf(
+			/* translators: %1$s is replaced with opening link tag taking you to hostgator.com/wordpress, %2$s is replaced with closing link tag, %3$s is replaced with opening link tag taking you to login page, %4$s is replaced with closing link tag, %5$s is replaced with opening link tag taking you to portal.hostgator.com, %6$s is replaced with closing link tag */
+			esc_html__( 'A %1$sHostGator%2$s powered website. Is this your website? Log in to %3$sWordPress%4$s or %5$sHostgator%6$s.', 'wp-plugin-hostgator' ) . '&nbsp;',
+			'<a href="' . esc_url( 'https://www.hostgator.com/managed-wordpress-hosting' ) . '" target="_blank" rel="noopener noreferrer nofollow">',
+			'</a>',
+			'<a href="' . esc_url( wp_login_url() ) . '">',
+			'</a>',
+			'<a href="' . esc_url( 'https://portal.hostgator.com/' ) . '" target="_blank" rel="noopener noreferrer nofollow">',
+			'</a>'
+		),
+		'template_page_title' => sprintf(
+			/* translators: %s: Blog name */
+			__( '%s &mdash; Coming Soon', 'wp-plugin-hostgator' ),
+			esc_html( get_option( 'blogname' ) )
+		),
+		'admin_bar_text'      => '<div style="background-color: #ffcf00; color: #191936; padding: 0 1rem;">' . __( 'Coming Soon Active', 'wp-plugin-hostgator' ) . '</div>',
+		'admin_notice_text'   => sprintf(
+			/* translators: %1$s is replaced with the opening link tag and %2$s is replaced with the closing link tag, %3$s is the opening link tag to preview the page, %4$s is the closing link tag. */
+			__( 'Your site is currently displaying a %1$scoming soon page%2$s. Once you are ready, %3$slaunch your site%4$s.', 'wp-plugin-hostgator' ),
+			'<a href="' . get_home_url() . '?preview=coming_soon" title="' . __( 'Preview the coming soon landing page', 'wp-plugin-hostgator' ) . '">',
+			'</a>',
+			'<a href="' . esc_url( admin_url( 'admin.php?page=hostgator#/home' ) ) . '">',
+			'</a>'
+		),
+		'template_styles'     => esc_url( HOSTGATOR_PLUGIN_URL . 'assets/styles/coming-soon.css' ),
+	)
+);
+setContainer( $nfd_module_container );
+
+
 $updateurl     = 'https://hiive.cloud/workers/release-api/plugins/newfold-labs/wp-plugin-hostgator'; // Custom API GET endpoint
 $pluginUpdater = new PluginUpdater( HOSTGATOR_PLUGIN_FILE, $updateurl );
 $pluginUpdater->setDataMap(
@@ -53,7 +114,6 @@ if ( is_admin() ) {
 require HOSTGATOR_PLUGIN_DIR . '/inc/Admin.php';
 require HOSTGATOR_PLUGIN_DIR . '/inc/AdminBar.php';
 require HOSTGATOR_PLUGIN_DIR . '/inc/base.php';
-require HOSTGATOR_PLUGIN_DIR . '/inc/coming-soon.php';
 require HOSTGATOR_PLUGIN_DIR . '/inc/jetpack.php';
 require HOSTGATOR_PLUGIN_DIR . '/inc/partners.php';
 require HOSTGATOR_PLUGIN_DIR . '/inc/performance.php';
