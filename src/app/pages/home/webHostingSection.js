60613a666c5bd08af96336a47d0d4c3cb2b2867e
Evan Mullins <evanrm@gmail.com>
2022-04-07 18:10:51 -0700

    load region links in home hosting section

--- src/app/pages/home/webHostingSection.js
+++ src/app/pages/home/webHostingSection.js
@@ -8,6 +8,7 @@ import {
 	CardFooter,
 	Dashicon,
 } from '@wordpress/components';
+import { getLinkPerRegion, supportsLinkPerRegion } from '../../util/helpers';
 
 const WebHostingSection = () => {
 	return (
@@ -29,93 +30,96 @@ const WebHostingSection = () => {
 						)}
 					</p>
 				</CardHeader>
-				<CardFooter>
-					<div className="hgwp-cardlist-content">
-						<Heading level="4">
-							<Dashicon icon="desktop" />{' '}
+				{ supportsLinkPerRegion( 'home_manage_sites' ) &&
+					<CardFooter>
+						<div className="hgwp-cardlist-content">
+							<Heading level="4">
+								<Dashicon icon="desktop" />{' '}
+								{__('Manage Sites', 'wp-plugin-hostgator')}
+							</Heading>
+							<p>
+								{__(
+									'Manage your site from the control panel. You can create backups, set security, and improve performance.',
+									'wp-plugin-hostgator'
+								)}
+							</p>
+						</div>
+						<Button
+							variant="primary"
+							href={
+								getLinkPerRegion(
+									'home_manage_sites', 
+									null, 
+									__('Manage Sites', 'wp-plugin-hostgator')
+								)
+							}
+							target="_blank"
+							icon="desktop"
+						>
 							{__('Manage Sites', 'wp-plugin-hostgator')}
-						</Heading>
-						<p>
-							{__(
-								'Manage your site from the control panel. You can create backups, set security, and improve performance.',
-								'wp-plugin-hostgator'
-							)}
-						</p>
-					</div>
-					<Button
-						variant="primary"
-						href={
-							`https://portal.hostgator.com/packages?` +
-							`&utm_campaign=` +
-							`&utm_content=home_hosting_sites_link` +
-							`&utm_term=manage_sites` +
-							`&utm_medium=brand_plugin` +
-							`&utm_source=wp-admin/admin.php?page=hostgator#/home`
-						}
-						target="_blank"
-						icon="desktop"
-					>
-						{__('Manage Sites', 'wp-plugin-hostgator')}
-					</Button>
-				</CardFooter>
-				<CardFooter>
-					<div className="hgwp-cardlist-content">
-						<Heading level="4">
-							<Dashicon icon="email" />{' '}
-							{__('Email', 'wp-plugin-hostgator')}
-						</Heading>
-						<p>
-							{__(
-								'Create email accounts, compose, send, and receive your email from the control panel.',
-								'wp-plugin-hostgator'
-							)}
-						</p>
-					</div>
-					<Button
-						variant="primary"
-						href={
-							`https://portal.hostgator.com/email?` +
-							`&utm_campaign=` +
-							`&utm_content=home_hosting_email_link` +
-							`&utm_term=manage_email` +
-							`&utm_medium=brand_plugin` +
-							`&utm_source=wp-admin/admin.php?page=hostgator#/home`
-						}
-						target="_blank"
-						icon="email"
-					>
-						{__('Manage Email', 'wp-plugin-hostgator')}
-					</Button>
-				</CardFooter>
-				<CardFooter>
-					<div className="hgwp-cardlist-content">
-						<Heading level="4">
-							<Dashicon icon="admin-site" />{' '}
-							{__('Domains', 'wp-plugin-hostgator')}
-						</Heading>
-						<p>
-							{__(
-								'Find a new domain and assign it to your site or start a new site with a fresh domain.',
-								'wp-plugin-hostgator'
-							)}
-						</p>
-					</div>
-					<Button
-						variant="secondary"
-						href={
-							`https://portal.hostgator.com/domain/purchase/registration?` +
-							`&utm_campaign=` +
-							`&utm_content=home_hosting_domain_link` +
-							`&utm_term=find_domain` +
-							`&utm_medium=brand_plugin` +
-							`&utm_source=wp-admin/admin.php?page=hostgator#/home`
-						}
-						target="_blank"
-						icon="admin-site"
-					>
-						{__('Find a Domain', 'wp-plugin-hostgator')}
-					</Button>
-				</CardFooter>
+						</Button>
+					</CardFooter>
+				}
+				{ supportsLinkPerRegion( 'home_manage_email' ) &&
+					<CardFooter>
+						<div className="hgwp-cardlist-content">
+							<Heading level="4">
+								<Dashicon icon="email" />{' '}
+								{__('Email', 'wp-plugin-hostgator')}
+							</Heading>
+							<p>
+								{__(
+									'Create email accounts, compose, send, and receive your email from the control panel.',
+									'wp-plugin-hostgator'
+								)}
+							</p>
+						</div>
+						<Button
+							variant="primary"
+							href={
+								getLinkPerRegion(
+									'home_manage_email', 
+									null, 
+									__('Manage Email', 'wp-plugin-hostgator')
+								)
+							}
+							target="_blank"
+							icon="email"
+						>
+							{__('Manage Email', 'wp-plugin-hostgator')}
+						</Button>
+					</CardFooter> 
+				}
+				{ supportsLinkPerRegion( 'home_find_domain' ) &&
+					<CardFooter>
+						<div className="hgwp-cardlist-content">
+							<Heading level="4">
+								<Dashicon icon="admin-site" />{' '}
+								{__('Domains', 'wp-plugin-hostgator')}
+							</Heading>
+							<p>
+								{__(
+									'Find a new domain and assign it to your site or start a new site with a fresh domain.',
+									'wp-plugin-hostgator'
+								)}
+							</p>
+						</div>
+						<Button
+							variant="secondary"
+							href={
+								getLinkPerRegion(
+									'home_find_domain', 
+									null, 
+									__('Manage Email', 'wp-plugin-hostgator')
+								)
+							}
+							target="_blank"
+							icon="admin-site"
+						>
+							{__('Find a Domain', 'wp-plugin-hostgator')}
+						</Button>
+					</CardFooter>
+				}
 				<CardFooter>
 					<div className="hgwp-cardlist-content">
 						<Heading level="4">
