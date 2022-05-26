861d8b39091959eea24871ccf686176b5df8ca35
Evan Mullins <evanrm@gmail.com>
2022-05-26 12:06:42 -0400

    update build to be more i18n friendly
    don't concatenate modules
    set terser mangle reserved options
    exclude src files in i18n make commands
    preserve translator comments in js
    make new pot
    update pt_BR po file to reflect new pot

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const { merge } = require('webpack-merge');
 const wpScriptsConfig = require('@wordpress/scripts/config/webpack.config');
 const version = require('./package.json').version;
 const { ProvidePlugin } = require('webpack');
+const TerserPlugin = require( 'terser-webpack-plugin' );
+
 /**
  * Aliases for resolving Brand imports
  */
@@ -31,6 +33,42 @@ const mostCommonImports = {
     sprintf: ['@wordpress/i18n', 'sprintf'],
     classNames: ['classnames'],
 };
+
+/**
+ * Terser's function to decide which comments to preserve.
+ *
+ * @see https://github.com/Automattic/jetpack/blob/fdf3b72390c7fcb64508d985149de2af01b935b3/projects/js-packages/webpack-config/src/webpack/terser.js
+ * @see https://github.com/terser/terser/blob/v5.9.0/lib/output.js#L171-L177
+ * @param {object} comment - Comment object.
+ * @param {string} comment.type - Comment type.
+ * @param {string} comment.value - Comment text.
+ * @returns {boolean} Whether to keep it.
+ */
+ function isSomeComments( comment ) {
+	return (
+		( comment.type === 'comment2' || comment.type === 'comment1' ) &&
+		/@preserve|@lic|@cc_on|^\**!/i.test( comment.value )
+	);
+}
+
+/**
+ * Function to match a WP i18n "translators" comment.
+ *
+ * @see https://github.com/Automattic/jetpack/blob/fdf3b72390c7fcb64508d985149de2af01b935b3/projects/js-packages/webpack-config/src/webpack/terser.js
+ * @see https://github.com/php-gettext/Gettext/blob/4.x/src/Utils/ParsedComment.php#L53-L73
+ * @see https://github.com/wp-cli/i18n-command/blob/v2.2.9/src/JsCodeExtractor.php#L15
+ * @param {object} comment - Comment object.
+ * @param {string} comment.type - Comment type.
+ * @param {string} comment.value - Comment text.
+ * @returns {boolean} Whether to keep it.
+ */
+ function isTranslatorsComment( comment ) {
+	return (
+		( comment.type === 'comment2' || comment.type === 'comment1' ) &&
+		/^[#*/ \t\r\n]*[tT]ranslators/.test( comment.value )
+	);
+}
+
 /**
  * Extend @wordpress/scripts default webpack config with:
  * - Versioned build folder (via package.json version) for cache-busting.
@@ -45,5 +83,35 @@ const hostgatorConfig = {
     },
     resolve: { alias },
     plugins: [new ProvidePlugin(mostCommonImports)],
+    optimization: {
+        concatenateModules: false,
+        minimize: true,
+        minimizer: [
+            new TerserPlugin( {
+                terserOptions: {
+                    mangle: {
+                        reserved: [ '__', '_n', '_nx', '_x' ]
+                    },
+                    format: {
+                        // The `new Function` bit here is a hack to work around the way terser-webpack-plugin serializes
+                        // the terserOptions. The "comments" function must not refer to anything from the local or global scope,
+                        // so we "paste" our external functions inside.
+                        comments: new Function(
+                            'node',
+                            'comment',
+                            `${ isTranslatorsComment }; return isTranslatorsComment( comment )`
+                        ),
+                    },
+                },
+                // Same.
+                extractComments: new Function(
+                    'node',
+                    'comment',
+                    `${ isSomeComments }; return isSomeComments( comment )`
+                ),
+            }),
+        ]
+    },
+
 };
 module.exports = merge(wpScriptsConfig, hostgatorConfig);
