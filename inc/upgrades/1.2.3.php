78356dca5c21db38658ed53d34e1e73cdcb5237c
Evan Mullins <evanrm@gmail.com>
2022-11-15 16:41:48 -0500

    fix package name in upgrade routine

--- inc/upgrades/1.2.3.php
+++ inc/upgrades/1.2.3.php
@@ -2,7 +2,7 @@
 /**
  * Handle updates for version 1.2.3
  *
- * @package Bluehost
+ * @package HostGatorWordPressPlugin
  */
 
 // Clear newfold_marketplace transient with update in wp-module-marketplace@1.4.0
