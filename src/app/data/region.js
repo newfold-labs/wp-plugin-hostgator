91474c8f67e2da1b7e11cf072d6610f046a80c75
Evan Mullins <evanrm@gmail.com>
2023-01-06 12:01:14 -0500

    Add/spanish translations (#52)
    * add translation files for es_MX
    
    * force all Spanish languages to use plugin translations for MX
    
    * use new wrapper method to clean up locale overrides
    
    * lint fixes
    
    * adding region links for MX CL and CO locales
    
    * break out the locale_overrides static function for clarity
    
    * swap out the bluehost wp-php-standards for newfold replacement
    
    * update phpcs ruleset names
    
    * Update to make sure PHPCS dependencies are properly installed
    
    Co-authored-by: Micah Wood <micah@wpscholar.com>

--- src/app/data/region.js
+++ src/app/data/region.js
@@ -23,43 +23,70 @@ const region = {
         BR: 'https://www.hostgator.com.br',
         MX: 'https://www.hostgator.mx',
         CL: 'https://www.hostgator.cl',
-        CO: 'https://www.hostgator.co'
+        CO: 'https://www.hostgator.co',
     },
     home_manage_sites: {
         default: 'https://portal.hostgator.com/packages',
         BR: 'https://cliente.hostgator.com.br/',
+        MX: 'https://cliente.hostgator.mx/',
+        CL: 'https://cliente.hostgator.cl/',
+        CO: 'https://cliente.hostgator.co/',
     },
     home_manage_email: {
         default: 'https://portal.hostgator.com/email',
         BR: 'https://cliente.hostgator.com.br/emails-list',
+        MX: 'https://cliente.hostgator.mx/emails-list',
+        CL: 'https://cliente.hostgator.cl/emails-list',
+        CO: 'https://cliente.hostgator.co/emails-list',
     },
     home_find_domain: {
         default: 'https://portal.hostgator.com/email',
         BR: 'https://www.hostgator.com.br/registro-de-dominio/',
+        MX: 'https://www.hostgator.mx/dominios/',
+        CL: 'https://www.hostgator.cl/dominios/',
+        CO: 'https://www.hostgator.co/dominios/',
     },
     help_phone_number: {
         default: 'tel:8669642867',
         BR: 'false', // a string false value will remove this section
+        MX: 'false', // a string false value will remove this section
+        CL: 'false', // a string false value will remove this section
+        CO: 'false', // a string false value will remove this section
     },
     help_chat: {
         default: 'https://helpchat.hostgator.com/',
         BR: 'https://financeiro.hostgator.com.br/chat/?country=br&department=technical', // ideally to be open in a popup, not new tab
+        MX: 'https://billing.hostgator.mx/chat/?country=mx&department=technical',
+        CL: 'https://billing.hostgator.mx/chat/?country=cl&department=technical',
+        CO: 'https://billing.hostgator.mx/chat/?country=co&department=technical',
     },
     help_twitter: {
         default: 'https://twitter.com/hgsupport',
         BR: 'https://twitter.com/hostgatorbrasil',
+        MX: 'https://twitter.com/HostGatorES',
+        CL: 'https://twitter.com/HostGatorES',
+        CO: 'https://twitter.com/HostGatorES',
     },
     help_kb: {
         default: 'https://www.hostgator.com/help',
         BR: 'https://suporte.hostgator.com.br/hc/pt-br',
+        MX: 'https://soporte-latam.hostgator.com/hc/es-419',
+        CL: 'https://soporte-latam.hostgator.com/hc/es-419',
+        CO: 'https://soporte-latam.hostgator.com/hc/es-419',
     },
     help_blog: {
         default: 'https://www.hostgator.com/blog/',
         BR: 'https://www.hostgator.com.br/blog/',
+        MX: 'https://www.hostgator.mx/blog/',
+        CL: 'https://www.hostgator.mx/blog/',
+        CO: 'https://www.hostgator.mx/blog/',
     },
     help_youtube: {
         default: 'https://www.youtube.com/c/hostgatorUS',
         BR: 'https://www.youtube.com/c/HostGatorBrasil',
+        MX: 'https://www.youtube.com/c/HostGatorM%C3%A9xico',
+        CL: 'https://www.youtube.com/c/HostGatorM%C3%A9xico',
+        CO: 'https://www.youtube.com/c/HostGatorM%C3%A9xico',
     },
 
 };
