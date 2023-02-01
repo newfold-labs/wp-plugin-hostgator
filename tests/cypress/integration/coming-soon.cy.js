6aa4048eaa0c789bd4ef6ebe5aece8ee5d7d4b6c
Evan Mullins <evanrm@gmail.com>
2023-02-01 17:38:16 -0500

    tweak tests

--- tests/cypress/integration/coming-soon.cy.js
+++ tests/cypress/integration/coming-soon.cy.js
@@ -13,13 +13,11 @@ describe('Coming Soon', function () {
 		cy.get('.coming-soon-toggle input[type="checkbox"]').uncheck();
 		cy.get('.coming-soon-toggle input[type="checkbox"]').check();
 		cy.wait(100);
-		cy
-			.get('.edit-site-notices .components-snackbar__content')
+		cy.get('.edit-site-notices .components-snackbar__content')
 			.contains('div', 'Coming soon activated')
 			.should('be.visible');
 
-		cy
-			.get('.coming-soon-protip .accordion-summary')
+		cy.get('.coming-soon-protip .accordion-summary')
 			.contains('p', 'Pro Tip')
 			.should('be.visible');
 		cy.get('.coming-soon-protip').should('not.have.attr', 'open');
@@ -31,8 +29,7 @@ describe('Coming Soon', function () {
 
 	it('Has Coming Soon Section on Home', () => {
 		cy.visit('/wp-admin/admin.php?page=hostgator#/home');
-		cy
-			.get('.hgwp-section-coming-soon')
+		cy.get('.hgwp-section-coming-soon')
 			.scrollIntoView()
 			.contains('h3', 'Coming Soon')
 			.should('be.visible');
@@ -41,16 +38,14 @@ describe('Coming Soon', function () {
 
 	it('Has Coming Soon in Admin Toolbar', () => {
 		cy.reload();
-		cy
-			.get('#wp-toolbar #wp-admin-bar-hostgator-coming_soon')
+		cy.get('#wp-toolbar #wp-admin-bar-hostgator-coming_soon')
 			.contains('div', 'Coming Soon Active')
 			.should('be.visible');
 	});
 
 	it('Displays admin coming soon notice', () => {
 		cy.visit('/wp-admin/index.php');
-		cy
-			.get('.notice-warning')
+		cy.get('.notice-warning')
 			.contains('p', 'coming')
 			.should('be.visible');
 	});
@@ -59,13 +54,11 @@ describe('Coming Soon', function () {
 		cy.visit('/wp-admin/admin.php?page=hostgator#/home');
 		cy.get('.hgwp-section-coming-soon button.is-primary').click();
 		cy.wait(500);
-		cy
-			.get('#wp-toolbar #wp-admin-bar-hostgator-coming_soon')
+		cy.get('#wp-toolbar #wp-admin-bar-hostgator-coming_soon')
 			.contains('div', 'Coming Soon Active')
 			.should('not.be.visible');
 
-		cy
-			.get('.hgwp-section-coming-soon')
+		cy.get('.hgwp-section-coming-soon')
 			.scrollIntoView()
 			.contains('h3', 'Site Launched')
 			.should('be.visible');
@@ -73,20 +66,25 @@ describe('Coming Soon', function () {
 		cy.get('.hgwp-section-coming-soon button.is-secondary').click();
 
 		
-		cy
-			.get('.hgwp-section-coming-soon')
+		cy.get('.hgwp-section-coming-soon')
 			.scrollIntoView()
 			.contains('h3', 'Coming Soon')
 			.should('be.visible');
 	});
 
 	it('Displays Coming Soon on Frontend', () => {
-		cy.get('#wp-admin-bar-logout a').click({ force: true });
+		cy.logout();
+		// cy.get('#wp-admin-bar-logout a').click({ force: true });
 		cy.visit('/');
-		cy
-			.get('body')
+		cy.get('body')
 			.contains('h2', 'Coming Soon')
 			.should('be.visible');
+	});
+
+	it('Coming soon page is Accessible', () => {
+		cy.injectAxe();
+		cy.wait(500);
+		cy.checkA11y('body');
 	});
 
 	it('Launching launches site', () => {
@@ -95,8 +93,7 @@ describe('Coming Soon', function () {
 		cy.get('.coming-soon-toggle input[type="checkbox"]').should('be.checked');
 
 		cy.visit('/wp-admin/admin.php?page=hostgator#/home');
-		cy
-			.get('.hgwp-section-coming-soon')
+		cy.get('.hgwp-section-coming-soon')
 			.scrollIntoView()
 			.contains('h3', 'Coming Soon')
 			.should('be.visible');
@@ -104,17 +101,18 @@ describe('Coming Soon', function () {
 		cy.get('.hgwp-section-coming-soon button.is-primary').click();
 		cy.get('.hgwp-section-coming-soon button.is-link').click(); //dismiss
 
-		cy
-			.get('.hgwp-section-coming-soon')
+		cy.get('.hgwp-section-coming-soon')
 			.should('not.exist');
 
-		cy.get('#wp-admin-bar-logout a').click({ force: true });
+		cy.logout();
+		// cy.get('#wp-admin-bar-logout a').click({ force: true });
 		cy.visit('/');
-		cy
-			.get('body')
+
+		cy.get('body')
 			.contains('h2', 'Coming Soon')
 			.should('not.exist');
 
 		cy.login(Cypress.env('wpUsername'), Cypress.env('wpPassword'));
+		cy.visit('/wp-admin/admin.php?page=hostgator#/settings');
 	})
 });
