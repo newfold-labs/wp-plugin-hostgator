0c6afe8d03333ed7cfe0f982a6860c21d5bff0d4
A. Alani <abdulrahmanalani95@gmail.com>
2023-07-06 13:32:26 -0700

    Help page
    fix spacing on some other files

--- src/app/pages/settings/index.js
+++ src/app/pages/settings/index.js
@@ -9,29 +9,29 @@ const Settings = () => {
 	return (
 		<Page title="Settings" className={"hgwp-app-settings-page"}>
 			<SectionContainer className={'hgwp-app-settings-container'}>
-                <SectionHeader
-                    title={__('Settings', 'wp-plugin-hostgator')}
-                    subTitle={__('This is where you can manage common settings for your website.', 'wp-plugin-hostgator')}
-                    className={'hgwp-app-settings-header'}
-                />
+				<SectionHeader
+					title={__('Settings', 'wp-plugin-hostgator')}
+					subTitle={__('This is where you can manage common settings for your website.', 'wp-plugin-hostgator')}
+					className={'hgwp-app-settings-header'}
+				/>
 
-                <SectionContent separator={true} className={'hgwp-app-settings-coming-soon'}>
-                    <ComingSoon />
-                </SectionContent>
+				<SectionContent separator={true} className={'hgwp-app-settings-coming-soon'}>
+					<ComingSoon />
+				</SectionContent>
 
 				<SectionContent separator={true} className={'hgwp-app-settings-update'}>
-                    <AutomaticUpdates />
-                </SectionContent>
+					<AutomaticUpdates />
+				</SectionContent>
 
-                <SectionContent separator={true} className={'hgwp-app-settings-content'}>
-                    <ContentSettings />
-                </SectionContent>
+				<SectionContent separator={true} className={'hgwp-app-settings-content'}>
+					<ContentSettings />
+				</SectionContent>
 
 				<SectionContent className={'hgwp-app-settings-comments'}>
-                    <CommentSettings />
-                </SectionContent>
+					<CommentSettings />
+				</SectionContent>
 
-            </SectionContainer>
+			</SectionContainer>
 		</Page>
 	);
 };
