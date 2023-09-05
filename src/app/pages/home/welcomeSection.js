b21e1056d6f6b2d21d09b6fa95e46d9eab28f490
Evan Mullins <evanrm@gmail.com>
2023-09-05 17:15:42 -0400

    fix welcome section onboarding screen missing props and bad callback definition

--- src/app/pages/home/welcomeSection.js
+++ src/app/pages/home/welcomeSection.js
@@ -1,4 +1,4 @@
-import {OnboardingScreen} from "@newfold-labs/wp-module-ecommerce";
+import { OnboardingScreen } from "@newfold-labs/wp-module-ecommerce";
 import { useNotification } from "../../components/notifications/feed";
 import AppStore from "../../data/store";
 import { useContext } from "@wordpress/element";
@@ -7,9 +7,21 @@ import { hostgatorSettingsApiFetch } from "../../util/helpers";
 const WelcomeSection = () => {
     const {store, setStore} = useContext(AppStore)
     const notify= useNotification();
-    const toggleComingSoon = hostgatorSettingsApiFetch(!store?.comingSoon, console.error, (response) => setStore(() => ({ ...store, comingSoon: !store?.comingSoon})));
+    const toggleComingSoon = () => 
+        hostgatorSettingsApiFetch(
+            { comingSoon: !store.comingSoon },
+            console.error,
+            () => setStore({ ...store, comingSoon: !store.comingSoon })
+    );
 
-    return <OnboardingScreen notify={notify} showShadowBox={false} comingSoon={toggleComingSoon} />
+    return (
+        <OnboardingScreen
+            comingSoon={store.comingSoon}
+            toggleComingSoon={toggleComingSoon}
+            notify={notify}
+            showShadowBox={false} 
+        />
+    );
 }
 
 export default WelcomeSection;
