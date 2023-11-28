7679d9359f2b5e77c9c139c3400876bc2a1b676c
Evan Mullins <evanrm@gmail.com>
2023-11-28 16:11:59 -0500

    update language test to check region values

--- tests/cypress/integration/help-region.cy.js
+++ tests/cypress/integration/help-region.cy.js
@@ -12,20 +12,23 @@ describe('Language updates on Help Page', function () {
 		});
 	});
 
-	it('Is Accessible', () => {
-		cy.injectAxe();
-		cy.wait(500);
-		cy.a11y('.hgwp-app-body');
-	});
-
 	it('Phone Card Exists', () => {
+		// redundant test from help.cy.js
 		cy.get('.card-help-phone').contains('h3', 'Phone')
 			.scrollIntoView()
 			.should('be.visible');
 	});
 
+	it('Tweet Card Exists', () => {
+		// redundant test from help.cy.js
+		cy.get('.card-help-twitter').contains('h3', 'Tweet')
+			.scrollIntoView()
+			.should('be.visible');
+	});
+
 	it('Language Change to pt_BR updates text', () => {
-        // cy.setRegion('BR');
+		cy.setBrand('hostgator-latam');
+        cy.setRegion('br');
         cy.setLanguage('pt_BR');
 		cy.visit('/wp-admin/admin.php?page=hostgator#/help', {
 			onBeforeLoad() {
@@ -38,6 +41,20 @@ describe('Language updates on Help Page', function () {
 		cy.get('.card-help-phone').contains('h3', 'Telefone')
 			.scrollIntoView()
 			.should('be.visible');
+	});
+
+	it('Phone Card Does Not Exist', () => {
+		// No phone card for help in hg-latam-br
+		cy.get('.card-help-phone').contains('h3', 'Phone')
+			.scrollIntoView()
+			.should('not-exist');
+	});
+
+	it('Twitter Card Does Not Exist', () => {
+		// No phone card for help in hg-latam-br
+		cy.get('.card-help-twitter').contains('h3', 'Phone')
+			.scrollIntoView()
+			.should('not-exist');
 	});
 
 	it('Chat Card Exists', () => {
@@ -59,17 +76,13 @@ describe('Language updates on Help Page', function () {
 	});
 
 	it('Language change back to en_US updates text back', () => {
-        // cy.setRegion();
+        cy.setBrand();
+		cy.setRegion();
         cy.setLanguage();
-        cy.visit('/wp-admin/admin.php?page=hostgator#/help', {
-			onBeforeLoad() {
-				cy.window().then((win) => {
-                    win.NewfoldRuntime.restUrl = "http://localhost:8880/index.php?rest_route=/";
-				});
-			}
-		});
+        cy.reload();
 
-		cy.get('.card-help-kb').contains('h3', 'Knowledge Base')
+		// check that page reloaded
+		cy.get('.card-help-blog').contains('h3', 'Blog')
 			.scrollIntoView()
 			.should('be.visible');
 	});
