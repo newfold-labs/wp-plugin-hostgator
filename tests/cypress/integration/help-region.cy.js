082aa5b3705fb8c9f2f9e6a8b5d72cabe225cf3a
Evan Mullins <evanrm@gmail.com>
2023-11-28 16:31:32 -0500

    cleanup help region test more

--- tests/cypress/integration/help-region.cy.js
+++ tests/cypress/integration/help-region.cy.js
@@ -3,13 +3,7 @@
 describe('Language updates on Help Page', function () {
 
 	before(() => {
-		cy.visit('/wp-admin/admin.php?page=hostgator#/help', {
-			onBeforeLoad() {
-				cy.window().then((win) => {
-                    win.NewfoldRuntime.restUrl = "http://localhost:8880/index.php?rest_route=/";
-				});
-			}
-		});
+		cy.visit('/wp-admin/admin.php?page=hostgator#/help');
 	});
 
 	it('Phone Card Exists', () => {
@@ -18,39 +12,22 @@ describe('Language updates on Help Page', function () {
 			.should('be.visible');
 	});
 
-	it('Tweet Card Exists', () => {
+	it('Twitter Card Exists', () => {
 		cy.get('.card-help-twitter')
 			.scrollIntoView()
 			.should('be.visible');
 	});
 
-	it('Language Change to pt_BR updates text', () => {
+	it('Region Change Updates Content', () => {
 		cy.setBrand('hostgator-latam');
         cy.setRegion('br');
         cy.setLanguage('pt_BR');
+		cy.reload();
 
-		cy.visit('/wp-admin/admin.php?page=hostgator#/help', {
-			onBeforeLoad() {
-				cy.window().then((win) => {
-                    win.NewfoldRuntime.restUrl = "http://localhost:8880/index.php?rest_route=/";
-				});
-			}
-		});
-
-		cy.get('.card-help-phone')
-			.scrollIntoView()
-			.should('be.visible');
-	});
-
-	it('Phone Card Does Not Exist', () => {
-		// No phone card for help in hg-latam-br
 		cy.get('.card-help-phone')
 			.scrollIntoView()
 			.should('not.exist');
-	});
 
-	it('Twitter Card Does Not Exist', () => {
-		// No twitter card for help in hg-latam-br
 		cy.get('.card-help-twitter')
 			.scrollIntoView()
 			.should('not.exist');
