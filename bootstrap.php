6cfd7503f4aa41d3ac63a93e371cf8314c56d494
Evan Mullins <evanrm@gmail.com>
2024-05-17 14:28:59 -0400

    revert text change

--- bootstrap.php
+++ bootstrap.php
@@ -19,7 +19,7 @@ if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 	require __DIR__ . '/vendor/autoload.php';
 } else {
 	if ( 'local' === wp_get_environment_type() ) {
-		wp_die( esc_html( __( 'TESTINGONLY Please install the HostGator Plugin dependencies.', 'wp-plugin-hostgator' ) ) );
+		wp_die( esc_html( __( 'Please install the HostGator Plugin dependencies.', 'wp-plugin-hostgator' ) ) );
 	}
 	return;
 }
