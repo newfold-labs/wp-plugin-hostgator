216f6f8fabc4d7a8c3f8ee432fe7227311712c8c
wpalani <abdulrahmanalani95@gmail.com>
2024-07-24 17:30:21 -0700

    Update test matrix
    - Update plugin requirements header
    - Update cypress action

--- wp-plugin-hostgator.php
+++ wp-plugin-hostgator.php
@@ -13,9 +13,9 @@
  * Update URI:        https://github.com/newfold-labs/wp-plugin-hostgator/
  * Description:       WordPress plugin that integrates a WordPress site with the HostGator control panel, including performance, security, and update features.
  * Version:           2.10.3
- * Requires at least: 6.3
+ * Requires at least: 6.4
  * Requires PHP:      7.3
- * Tested up to:      6.6
+ * Tested up to:      6.6.1
  * Author:            HostGator
  * Author URI:        https://hostgator.com
  * Text Domain:       wp-plugin-hostgator
