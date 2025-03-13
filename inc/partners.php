82bd6132ef74554cacb6a8c04137782da961ceb5
Evan Mullins <evanrm@gmail.com>
2025-03-13 11:59:07 -0400

    pause partner sas integrations

--- inc/partners.php
+++ inc/partners.php
@@ -18,7 +18,7 @@ function wpforms_upgrade_affiliate_link( $url ) {
 	return 'http://www.shareasale.com/r.cfm?B=837827&U=1258907&M=64312&urllink=' . rawurlencode( $url );
 }
 
-add_filter( 'wpforms_upgrade_link', __NAMESPACE__ . '\\wpforms_upgrade_affiliate_link' );
+// add_filter( 'wpforms_upgrade_link', __NAMESPACE__ . '\\wpforms_upgrade_affiliate_link' );
 
 /**
  * All-in-One SEO upgrade affiliate link.
@@ -31,4 +31,4 @@ function aioseo_upgrade_affiliate_link( $url ) {
 	return 'http://www.shareasale.com/r.cfm?B=1491200&U=1258907&M=94778&urllink=' . rawurlencode( $url );
 }
 
-add_filter( 'aioseo_upgrade_link', __NAMESPACE__ . '\\aioseo_upgrade_affiliate_link' );
+// add_filter( 'aioseo_upgrade_link', __NAMESPACE__ . '\\aioseo_upgrade_affiliate_link' );
