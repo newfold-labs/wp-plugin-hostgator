9859907996b5483a05fd5ad13467b1a1f3e19c3f
Evan Mullins <evanrm@gmail.com>
2025-05-02 15:09:28 -0400

    dependency updates - updates wp-scripts to 30 which drops support for WP6.5 and lower

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './webpack-public-path';
 
 import App from './app';
 import domReady from '@wordpress/dom-ready';
-import { createRoot, render } from '@wordpress/element';
+import { createRoot } from '@wordpress/element';
 
 const WP_ADM_PAGE_ROOT_ELEMENT = 'hwa-app';
 const HG_ASCI = `                                                                                                     
@@ -46,12 +46,7 @@ console.log( HG_ASCI );
 const HGWPRender = () => {
 	const DOM_ELEMENT = document.getElementById( WP_ADM_PAGE_ROOT_ELEMENT );
 	if ( null !== DOM_ELEMENT ) {
-		if ( 'undefined' !== typeof createRoot ) {
-			// WP 6.2+ only
 			createRoot( DOM_ELEMENT ).render( <App /> );
-		} else if ( 'undefined' !== typeof render ) {
-			render( <App />, DOM_ELEMENT );
-		}
 	}
 };
 
