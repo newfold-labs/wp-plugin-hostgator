e590ce995a92d81a5292909a0d62a7d03aea95af
Evan Mullins <evanrm@gmail.com>
2025-06-02 13:37:19 -0400

    finish adding edited files for removing cachingcontroller

--- bootstrap.php
+++ bootstrap.php
@@ -175,7 +175,6 @@ require HOSTGATOR_PLUGIN_DIR . '/inc/base.php';
 require HOSTGATOR_PLUGIN_DIR . '/inc/jetpack.php';
 require HOSTGATOR_PLUGIN_DIR . '/inc/LoginRedirect.php';
 require HOSTGATOR_PLUGIN_DIR . '/inc/partners.php';
-require HOSTGATOR_PLUGIN_DIR . '/inc/RestApi/CachingController.php';
 require HOSTGATOR_PLUGIN_DIR . '/inc/RestApi/SettingsController.php';
 require HOSTGATOR_PLUGIN_DIR . '/inc/RestApi/rest-api.php';
 require HOSTGATOR_PLUGIN_DIR . '/inc/settings.php';
