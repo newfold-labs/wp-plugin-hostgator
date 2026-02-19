ac451ab986877b49e4e39c3eada9aa79b26f3c4f
Evan Mullins <evanrm@gmail.com>
2026-02-19 08:56:28 -0500

    lint

--- src/app/data/routes.js
+++ src/app/data/routes.js
@@ -14,15 +14,15 @@ import Help from '../pages/help';
 import Admin from '../pages/admin';
 import { getMarketplaceSubnavRoutes } from '@modules/wp-module-marketplace/components/marketplaceSubnav';
 
-const addPartialMatch = (prefix, path) =>
-	prefix === path ? `${prefix}/*` : path;
+const addPartialMatch = ( prefix, path ) =>
+	prefix === path ? `${ prefix }/*` : path;
 
-function helpAction(e, preventDefault = false) {
+function helpAction( e, preventDefault = false ) {
 	if (
-		NewfoldRuntime.hasCapability('canAccessHelpCenter') &&
+		NewfoldRuntime.hasCapability( 'canAccessHelpCenter' ) &&
 		window.NewfoldFeatures?.features?.helpCenter
 	) {
-		if (preventDefault) {
+		if ( preventDefault ) {
 			e.preventDefault();
 		}
 		window.newfoldEmbeddedHelp.toggleNFDLaunchedEmbeddedHelp();
@@ -58,29 +58,29 @@ const PerformanceRedirect = () => {
 export const AppRoutes = () => {
 	return (
 		<Routes>
-			{routes.map(
-				(page) =>
+			{ routes.map(
+				( page ) =>
 					true === page.condition && (
 						<Route
 							end
-							key={page.name}
-							path={addPartialMatch('/marketplace')}
-							element={<page.Component />}
+							key={ page.name }
+							path={ addPartialMatch( '/marketplace' ) }
+							element={ <page.Component /> }
 						/>
 					)
-			)}
-			<Route path="/staging" element={<StagingRedirect />} />
-			<Route path="/performance" element={<PerformanceRedirect />} />
-			<Route path="/" element={<Home />} />
+			) }
+			<Route path="/staging" element={ <StagingRedirect /> } />
+			<Route path="/performance" element={ <PerformanceRedirect /> } />
+			<Route path="/" element={ <Home /> } />
 			<Route
 				path="*"
 				element={
-					<main style={{ padding: '1rem' }}>
+					<main style={ { padding: '1rem' } }>
 						<p>
-							{__(
+							{ __(
 								"There's nothing here!",
 								'wp-plugin-hostgator'
-							)}
+							) }
 						</p>
 					</main>
 				}
@@ -92,35 +92,35 @@ export const AppRoutes = () => {
 export const routes = [
 	{
 		name: '/home',
-		title: __('Home', 'wp-plugin-hostgator'),
+		title: __( 'Home', 'wp-plugin-hostgator' ),
 		Component: Home,
 		Icon: HomeIcon,
 		condition: true,
 	},
 	{
 		name: '/settings',
-		title: __('Manage WordPress', 'wp-plugin-hostgator'),
+		title: __( 'Manage WordPress', 'wp-plugin-hostgator' ),
 		Component: Settings,
 		Icon: AdjustmentsHorizontalIcon,
 		condition: true,
 	},
 	{
 		name: '/settings/staging',
-		title: __('Staging', 'wp-plugin-hostgator'),
+		title: __( 'Staging', 'wp-plugin-hostgator' ),
 		Component: Settings,
 		Icon: AdjustmentsHorizontalIcon,
 		condition: true,
 	},
 	{
 		name: '/settings/performance',
-		title: __('Performance', 'wp-plugin-hostgator'),
+		title: __( 'Performance', 'wp-plugin-hostgator' ),
 		Component: Settings,
 		Icon: AdjustmentsHorizontalIcon,
 		condition: true,
 	},
 	{
 		name: '/marketplace',
-		title: __('Marketplace', 'wp-plugin-hostgator'),
+		title: __( 'Marketplace', 'wp-plugin-hostgator' ),
 		Component: Marketplace,
 		Icon: ShoppingBagIcon,
 		subRoutes: await getMarketplaceSubnavRoutes(),
@@ -128,7 +128,7 @@ export const routes = [
 	},
 	{
 		name: '/help',
-		title: __('Help', 'wp-plugin-hostgator'),
+		title: __( 'Help', 'wp-plugin-hostgator' ),
 		Component: Help,
 		Icon: QuestionMarkCircleIcon,
 		condition: true,
@@ -136,20 +136,15 @@ export const routes = [
 	},
 	{
 		name: '/admin',
-		title: __('Admin', 'wp-plugin-hostgator'),
+		title: __( 'Admin', 'wp-plugin-hostgator' ),
 		Component: Admin,
 		condition: true,
 	},
 ];
 
-const topRoutePaths = [
-	'/home',
-	'/settings',
-	'/marketplace',
-	'/help',
-];
-export const topRoutes = _filter(routes, (route) =>
-	topRoutePaths.includes(route.name)
+const topRoutePaths = [ '/home', '/settings', '/marketplace', '/help' ];
+export const topRoutes = _filter( routes, ( route ) =>
+	topRoutePaths.includes( route.name )
 );
 
 export default AppRoutes;
