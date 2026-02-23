fc1fa1b0a26fd1fb6b57d0ed0627da2eaaea785d
Evan Mullins <evanrm@gmail.com>
2026-02-23 16:54:09 -0500

    use coming soon portal on plugin app home

--- src/app/pages/home/index.js
+++ src/app/pages/home/index.js
@@ -6,54 +6,71 @@ import WebHostingSection from './webHostingSection';
 import WebinarsBanner from 'App/components/webinars-banner';
 
 const Home = () => {
+
+	useEffect(() => {
+		// run when mounts
+		const comingSoonPortal =
+			document.getElementById('coming-soon-portal');
+
+		if (comingSoonPortal) {
+			window.NFDPortalRegistry.registerPortal(
+				'coming-soon',
+				comingSoonPortal
+			);
+		}
+
+		// run when unmounts
+		return () => {
+			window.NFDPortalRegistry.unregisterPortal('coming-soon');
+		};
+	}, []);
+
 	return (
 		<Page
 			title="Settings"
-			className={ 'hgwp-app-home-page hgwp-home xl:nfd-max-w-screen-lg' }
+			className={'hgwp-app-home-page hgwp-home xl:nfd-max-w-screen-lg'}
 		>
 			<div className="nfd-home__title-section">
 				<Title className="nfd-mb-1 nfd-font-bold">
-					{ __( 'Welcome to HostGator', 'wp-plugin-hostgator' ) }
+					{__('Welcome to HostGator', 'wp-plugin-hostgator')}
 				</Title>
 				<span className="nfd-text-sm">
-					{ __(
+					{__(
 						"We're very excited to get started with you!",
 						'wp-plugin-hostgator'
-					) }
+					)}
 				</span>
 			</div>
 
+			<Container className="nfd-max-w-full nfd-p-8">
+				<div id="coming-soon-portal" />
+			</Container>
+
 			<WebinarsBanner />
 
-			<Container className={ 'hgwp-app-home-container nfd-max-w-full' }>
+			<Container className={'hgwp-app-home-container nfd-max-w-full'}>
 				<Container.Header
-					title={ __( 'More', 'wp-plugin-hostgator' ) }
-					className={ 'hgwp-app-home-header display-none' }
+					title={__('Quick Links', 'wp-plugin-hostgator')}
+					className={'hgwp-app-home-header display-none'}
 				/>
-				<Container.Block
-					separator={ true }
-					className={ 'hgwp-app-home-coming-soon' }
-				>
-					<ComingSoon />
-				</Container.Block>
 
 				<Container.Block
-					separator={ true }
-					className={ 'hgwp-app-home-content' }
+					separator={true}
+					className={'hgwp-app-home-content'}
 				>
 					<WebContentSection />
 				</Container.Block>
 
 				<Container.Block
-					separator={ true }
-					className={ 'hgwp-app-home-settings' }
+					separator={true}
+					className={'hgwp-app-home-settings'}
 				>
 					<SettingsSection />
 				</Container.Block>
 
 				<Container.Block
-					separator={ false }
-					className={ 'hgwp-app-home-hosting' }
+					separator={false}
+					className={'hgwp-app-home-hosting'}
 				>
 					<WebHostingSection />
 				</Container.Block>
