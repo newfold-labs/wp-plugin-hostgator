d20ac032f659e71c88511fed686f7419ee9bb40c
Evan Mullins <evanrm@gmail.com>
2026-02-25 09:49:30 -0500

    fix appnav hosting url

--- src/app/components/app-nav/index.js
+++ src/app/components/app-nav/index.js
@@ -98,9 +98,7 @@ export const AppNavMenu = () => {
 				<Button
 					as="a"
 					className="nfd-flex nfd-gap-2 nfd-mr-4 nfd-text-nowrap"
-					href={ addUtmParams(
-						getPlatformPathUrl( 'hosting/details', 'app/#/sites' )
-					) }
+					href={ addUtmParams( 'https://www.hostgator.com/my-account/hosting/details/sites' ) }
 					variant="secondary"
 				>
 					{ __( 'Hosting Panel', 'wp-plugin-hostgator' ) }
